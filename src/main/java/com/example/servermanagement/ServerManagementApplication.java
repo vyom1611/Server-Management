fda52a46d0c53ee65466ae5703615f4e358fd7d6
vyom1611 <sharm843@umn.edu>
2022-09-26 19:20:28 -0500

    Init

---
+++ src/main/java/com/example/servermanagement/ServerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.servermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerManagementApplication.class, args);
+    }
+
+}
